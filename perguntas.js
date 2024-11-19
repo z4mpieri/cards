9dd646f0ae8d2ae77124c07725127666f9fe3a5c
Esther <00001091285287sp@al.educacao.sp.gov.br>
2024-11-19 18:41:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Alimentos',
+    'Tomate é fruta?',
+    'Sim, o tomate é uma fruta'
+)
+
+criaCartao(
+    'Geografia',
+    'Você sabia?',
+    'A Islândia cresce 5 centímetros por ano por causa de suas placas tectônicas em movimento.'
+)
+
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    '“Je pense, donc je suis” é a frase original, escrita em francês, do filósofo René Descartes (1596-1650). Ela resume o pensamento e o método de Descartes, para quem tudo tem início na dúvida.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia.'
+)
